fc9a3fc3d72e5620a1a400bfc518b21e7bc3e83f
YuraSich <metelkiny98@gmail.com>
2025-02-24 23:16:14 +0300

    добавил аналог collapsed для превью

---
+++ MarkdownEditor/Converters/BoolToGridLengthConverter .cs
@@ -0,0 +1,20 @@
+﻿using Avalonia.Controls;
+using Avalonia.Data.Converters;
+using System;
+using System.Globalization;
+
+namespace MarkdownEditor.Converters
+{
+    public class BoolToGridLengthConverter : IValueConverter
+    {
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            return value is false ? new GridLength(0) : new GridLength(1, GridUnitType.Star);
+        }
+
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
